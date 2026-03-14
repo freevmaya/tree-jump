1561d1c04ee04dcd7e67861d966ccc6bd6e6583e
Vadim Frolov <@vmaya>
2026-03-14 12:45:13 +0500

    requlary

--- public/test.php
+++ public/test.php
@@ -9,47 +9,6 @@
             .then((response)=>{
                 console.log(response);
             });
-
-        console.log("App starting...");
-        console.log("Current URL:", window.location.href);
-        console.log("In iframe:", window !== window.top);
-        console.log("Referrer:", document.referrer);
-
-        // Перехват всех редиректов
-        const originalPushState = history.pushState;
-        history.pushState = function() {
-            console.log("pushState called:", arguments);
-            return originalPushState.apply(this, arguments);
-        };
-
-        window.addEventListener('beforeunload', function() {
-            console.log("beforeunload - страница пытается уйти");
-        });
-
-        // Мониторинг создания iframe
-        const originalCreateElement = document.createElement;
-        document.createElement = function(tagName) {
-            if (tagName.toLowerCase() === 'iframe') {
-                console.log("Создается iframe!", new Error().stack);
-            }
-            return originalCreateElement.call(this, tagName);
-        };
-
-        try {
-            // Ваш основной код
-            console.log("Ваше приложение инициализируется");
-            
-            vkBridge.send("VKWebAppInit", {})
-                .then((response) => {
-                    console.log("VKWebAppInit response:", response);
-                })
-                .catch((error) => {
-                    console.error("VKWebAppInit error:", error);
-                });
-                
-        } catch (error) {
-            console.error("Критическая ошибка:", error);
-        }
     </script>
 </body>
 </html>

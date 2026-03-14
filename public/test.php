f37c6ff9f6c1805580c82df802c0a1f794b0cb44
Vadim Frolov <@vmaya>
2026-03-14 10:45:51 +0500

    requlary

--- public/test.php
+++ public/test.php
@@ -1,9 +1,15 @@
 <head>
     <title>Test</title>
+    <script src="https://unpkg.com/@vkontakte/vk-bridge/dist/browser.min.js"></script>
 </head>
 <body>
     <h1>Test Page</h1>
     <script>
+        vkBridge.send("VKWebAppInit", {})
+            .then((response)=>{
+                console.log(response);
+            });
+
         console.log("Test page loaded");
     </script>
 </body>

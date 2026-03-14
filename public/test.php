1275c4864b24b15d2ad0f8797352fbc7727ce691
Vadim Frolov <@vmaya>
2026-03-14 13:20:43 +0500

    requlary

--- public/test.php
+++ public/test.php
@@ -1,14 +1,16 @@
 <head>
     <title>Test</title>
-    <script src="scripts/browser.min.js?v=123122"></script>
-</head>
-<body>
-    <h1 style="color:green">Test Page</h1>
+    <script src="https://unpkg.com/@vkontakte/vk-bridge/dist/browser.min.js"></script>
+
     <script>
-        vkBridge.send("VKWebAppInit", {})
+      // Sends event to client
+      vkBridge.send('VKWebAppInit')
             .then((response)=>{
                 console.log(response);
             });
     </script>
+</head>
+<body>
+    <h1 style="color:green">Test Page</h1>
 </body>
 </html>
